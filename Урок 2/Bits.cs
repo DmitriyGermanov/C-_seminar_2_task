14d63ae0d04c57a4098281e9e3e5e777b42fcc92
Dmitriy Germanov <seltis.gtn@gmail.com>
2024-05-29 22:09:25 +0300

    some changes

--- Урок 2/Bits.cs
+++ Урок 2/Bits.cs
@@ -1,15 +1,14 @@
-﻿namespace Task_00
+﻿namespace Task_00;
+
+internal class Bits
 {
-    internal class Bits
-    {
-        private long data;
-        public long Data { get => data; private set => data = value; }
+    private long data;
+    public long Data { get => data; private set => data = value; }
 
-        public Bits(long data) => this.data = data;
-        public static implicit operator Bits(int value) => new Bits(value);
-        public static implicit operator Bits(byte value) => new Bits(value);
-        public static implicit operator Bits(long value) => new Bits(value);
+    public Bits(long data) => this.data = data;
+    public static implicit operator Bits(int value) => new Bits(value);
+    public static implicit operator Bits(byte value) => new Bits(value);
+    public static implicit operator Bits(long value) => new Bits(value);
 
-        public override string? ToString() => data.ToString();
-    }
+    public override string? ToString() => data.ToString();
 }
