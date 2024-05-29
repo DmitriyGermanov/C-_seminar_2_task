75ed61f6ae5ea059f8574e33e02148b336334652
Dmitriy Germanov <seltis.gtn@gmail.com>
2024-05-29 22:07:55 +0300

    initial commit

---
+++ Урок 2/Bits.cs
@@ -0,0 +1,15 @@
+﻿namespace Task_00
+{
+    internal class Bits
+    {
+        private long data;
+        public long Data { get => data; private set => data = value; }
+
+        public Bits(long data) => this.data = data;
+        public static implicit operator Bits(int value) => new Bits(value);
+        public static implicit operator Bits(byte value) => new Bits(value);
+        public static implicit operator Bits(long value) => new Bits(value);
+
+        public override string? ToString() => data.ToString();
+    }
+}
---
+++ Урок 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using Task_00;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        long a = 5;
+        int b = 6;
+        byte c = 7;
+        Bits bits = a;
+        Bits bits2 = b;
+        Bits bits3 = c;
+        Console.WriteLine($"""
+                            Проверка неявного приведения типов: 
+                            long = {bits}
+                            int = {bits2}
+                            byte = {bits3} 
+                            """);
+    }
+}
